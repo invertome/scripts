b50b954c950df4b4baa9f3e0018c9594468f404e
InverTome <104219631+invertome@users.noreply.github.com>
2023-03-29 09:49:04 -0600

    Update correct_newick.py
    using a different approach

--- phylogenetics/correct_newick.py
+++ phylogenetics/correct_newick.py
@@ -2,44 +2,39 @@
 
 import re
 import argparse
-from ete3 import Tree
-
-def add_missing_node_names(newick_string):
-    counter = 1
-    while re.search(r'(?<=\))(?=:\d+\.\d+)', newick_string):
-        newick_string = re.sub(r'(?<=\))(?=:\d+\.\d+)', f'_missing_node_{counter}', newick_string, count=1)
-        counter += 1
-    return newick_string
+from Bio import Phylo
 
 def correct_newick_tree(input_file, output_file):
+    # Read the input Newick tree file
     with open(input_file, 'r') as f:
         tree_string = f.read()
 
-    tree_string = add_missing_node_names(tree_string)
+    # Find and correct redundant double-brackets
     corrected_tree_string = re.sub(r'\(\(([^\(\)]+)\)\)', r'(\1)', tree_string)
 
+    # Check if any more redundant double-brackets are found and correct them
     while re.search(r'\(\(([^\(\)]+)\)\)', corrected_tree_string):
         corrected_tree_string = re.sub(r'\(\(([^\(\)]+)\)\)', r'(\1)', corrected_tree_string)
 
     try:
-        original_tree = Tree(tree_string)
-        corrected_tree = Tree(corrected_tree_string)
+        # Parse the original and corrected Newick trees
+        original_tree = Phylo.read(input_file, 'newick')
+        corrected_tree = Phylo.read(StringIO(corrected_tree_string), 'newick')
 
-        if original_tree.robinson_foulds(corrected_tree, unrooted_trees=True)[0] == 0:
-            with open(output_file, 'w') as f:
-                f.write(corrected_tree_string)
-            print(f"Corrected Newick tree written to {output_file}")
-        else:
-            print("Warning: Corrected tree topology differs from the original tree. Manual inspection is required.")
+        # Write the corrected tree to the output file
+        Phylo.write(corrected_tree, output_file, 'newick')
+        print(f"Corrected Newick tree written to {output_file}")
 
     except Exception as e:
+        # Print error message if there's an issue while parsing the trees
         print(f"Error while parsing trees: {str(e)}")
         print("Manual inspection of the input file is required.")
 
+# Set up command-line argument parsing
 parser = argparse.ArgumentParser(description='Correct Newick tree syntax issues')
 parser.add_argument('-i', '--input', type=str, required=True, help='Path to input Newick tree file')
 parser.add_argument('-o', '--output', type=str, required=True, help='Path to corrected output Newick tree file')
 args = parser.parse_args()
 
+# Call the correct_newick_tree function with the specified input and output file paths
 correct_newick_tree(args.input, args.output)
-
