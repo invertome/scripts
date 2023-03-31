55aec6769d918016f36e057f9c1957d83ffbbc9d
InverTome <104219631+invertome@users.noreply.github.com>
2023-03-31 09:48:32 -0600

    Create prompred_docker.py
    meme is difficult to compile due to perl dependencies, so this version uses a docker instead

---
+++ genomics/extractgene/prompred_docker.py
@@ -0,0 +1,72 @@
+import os
+import sys
+import argparse
+import subprocess
+from Bio import SeqIO
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
+
+def parse_fasta(file_path):
+    # ...
+
+def predict_promoter_regions(sequence, motif_file):
+    """
+    Use FIMO in a Docker container to predict promoter regions in the given sequence based on the provided motif file.
+
+    :param sequence: A SeqRecord object containing the sequence to analyze
+    :param motif_file: Path to the motif file
+    :return: List of (start, end) tuples representing promoter regions
+    """
+    # Write the sequence to a temporary FASTA file
+    with open("temp_sequence.fasta", "w") as temp_file:
+        SeqIO.write(sequence, temp_file, "fasta")
+
+    # Run FIMO within the Docker container
+    subprocess.run([
+        "docker", "run", "-v", f"{os.getcwd()}:/home/meme",
+        "memesuite/memesuite",
+        "fimo",
+        "--thresh", "0.0001",
+        "--oc", "/home/meme/fimo_out",
+        "/home/meme/" + motif_file,
+        "/home/meme/temp_sequence.fasta"
+    ])
+
+    os.remove("temp_sequence.fasta")
+
+    # ...
+
+def write_promoter_fasta(sequences, output_path):
+    """
+    Write a FASTA file containing the promoter sequences.
+
+    :param sequences: List of SeqRecords containing promoter sequences
+    :param output_path: Path to the output FASTA file
+    """
+    with open(output_path, "w") as output_file:
+        for seq in sequences:
+            SeqIO.write(seq, output_file, "fasta")
+
+if __name__ == "__main__":
+    # Parse command-line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--input", help="Path to the input FASTA file", required=True)
+    parser.add_argument("-o", "--output", help="Path to the output directory", required=True)
+    parser.add_argument("-m", "--motif", help="Path to the motif file", required=True)
+    args = parser.parse_args()
+
+    # Load sequences from the input FASTA file
+    sequences = parse_fasta(args.input)
+
+    # Predict and extract promoter regions for each sequence
+    promoter_sequences = []
+    for seq in sequences:
+        promoter_regions = predict_promoter_regions(seq, args.motif)
+        for start, end in promoter_regions:
+            promoter_seq = SeqRecord(seq.seq[start:end], id=f"{seq.id}_promoter", description=f"Promoter region for {seq.id}")
+            promoter_sequences.append(promoter_seq)
+
+    # Write the promoter sequences to the output FASTA file
+    output_fasta = os.path.join(args.output, "promoter_sequences.fasta")
+    os.makedirs(args.output, exist_ok=True)
+    write_promoter_fasta(promoter_sequences, output_fasta)
