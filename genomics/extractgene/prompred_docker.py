9bfb18d64b1754d1eb74309b31eaac07f9714b37
InverTome <104219631+invertome@users.noreply.github.com>
2023-04-01 14:17:38 -0600

    Update prompred_docker.py

--- genomics/extractgene/prompred_docker.py
+++ genomics/extractgene/prompred_docker.py
@@ -5,6 +5,7 @@ import subprocess
 import shutil
 import tempfile
 import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
 from Bio import SeqIO
 from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
