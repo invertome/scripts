61d364b7893690e77c0c2987b86bf2b12c157903
InverTome <104219631+invertome@users.noreply.github.com>
2023-04-18 10:28:33 -0600

    Update getgenbankspecies.py
    Fixed. Removed regex.

--- processing/getgenbankspecies.py
+++ processing/getgenbankspecies.py
@@ -1,21 +1,25 @@
 import argparse
 from Bio import SeqIO
-import re
 
 def parse_fasta_header(header):
-    pattern = r'>\S+\s.*\[(\S+\s\S+)\]'
-    match = re.search(pattern, header)
-    if match:
-        species = match.group(1).replace(" ", "_")
-        return species
+    if "[" in header and "]" in header:
+        identifier = header.split()[0].strip(">")
+        species_start = header.index("[") + 1
+        species_end = header.index("]")
+        species = header[species_start:species_end].replace(" ", "_")
+        return identifier, species
     return None
 
 def main(input_fasta, output_tsv):
     with open(input_fasta, "r") as fasta_file, open(output_tsv, "w") as tsv_file:
         for record in SeqIO.parse(fasta_file, "fasta"):
-            species = parse_fasta_header(record.description)
-            if species:
-                tsv_file.write(f"{record.id}\t{species}\n")
+            print(f"Processing header: {record.description}")  # Debug statement
+            parsed_header = parse_fasta_header(record.description)
+            if parsed_header:
+                identifier, species = parsed_header
+                tsv_file.write(f"{identifier}\t{species}\n")
+            else:
+                print(f"Skipped header: {record.description}")  # Debug statement
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Process a FASTA file and output a TSV file with specific fields.")
