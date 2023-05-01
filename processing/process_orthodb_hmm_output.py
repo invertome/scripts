0c04b2475e8cc8de988ca65159fcbd977b6d02a6
InverTome <104219631+invertome@users.noreply.github.com>
2023-05-01 11:02:45 -0600

    Update process_orthodb_hmm_output.py

--- processing/process_orthodb_hmm_output.py
+++ processing/process_orthodb_hmm_output.py
@@ -1,7 +1,16 @@
 import argparse
+import sys
 import subprocess
-import os
+from concurrent.futures import ThreadPoolExecutor
 from Bio import SeqIO
+
+# Check if the required package 'biopython' is installed
+try:
+    import Bio
+except ImportError:
+    print("Biopython package not found. Installing it now...")
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "biopython"])
+    import Bio
 
 # Replaces IDs in the results file with the species names
 def replace_ids(species_tab_file, results_file, output_file):
@@ -23,8 +32,7 @@ def replace_ids(species_tab_file, results_file, output_file):
                 outfile.write(line)
 
 # Extracts sequences from the database (or FASTA file) and writes them to an output FASTA file with updated headers
-def extract_fasta_sequences(database, fasta_file, species_tab_file, results_file, output_fasta_file):
-    # Collect target IDs from the results file
+def extract_fasta_sequences(fasta_file, species_tab_file, results_file, output_fasta_file, num_threads):
     target_ids = set()
     with open(results_file, 'r') as infile:
         for line in infile:
@@ -32,41 +40,28 @@ def extract_fasta_sequences(database, fasta_file, species_tab_file, results_file
                 target_id = line.split()[0]
                 target_ids.add(target_id)
 
-    # Create a dictionary mapping sequence IDs to species names
     id_to_species = {}
     with open(species_tab_file, 'r') as f:
         for line in f:
             _, id_, species, *rest = line.split()
-            id_to_species[id_] = species
+            id_to_species[id_] = species.replace(" ", "_")
 
-    if not database:
-        # Create BLAST database if not provided
-        database = "temp_blast_db"
-        subprocess.run(["makeblastdb", "-in", fasta_file, "-dbtype", "nucl", "-out", database])
+    indexed_fasta = SeqIO.index(fasta_file, "fasta")
+
+    def write_sequence(target_id):
+        record = indexed_fasta.get(target_id)
+        if record is not None:
+            species = id_to_species.get(target_id, target_id)
+            record.description = f"{record.description} {species}"
+            return record
 
     with open(output_fasta_file, 'w') as out_fasta:
-        for target_id in target_ids:
-            # Retrieve sequence from BLAST database
-            cmd = ["blastdbcmd", "-db", database, "-entry", target_id, "-outfmt", "%f"]
-            sequence = subprocess.run(cmd, stdout=subprocess.PIPE, text=True).stdout
+        with ThreadPoolExecutor(max_workers=num_threads) as executor:
+            records = list(executor.map(write_sequence, target_ids))
 
-            # Parse the retrieved sequence
-            record = SeqIO.read(io.StringIO(sequence), "fasta")
-
-            # Update the header with the species ID
-            species = id_to_species.get(record.id, record.id)
-            record.description = f"{record.description} {species}"
-
-            # Write the updated sequence to the output FASTA file
-            SeqIO.write(record, out_fasta, "fasta")
-
-    if not database:
-        # Remove temporary BLAST database files
-        for ext in [".nhr", ".nin", ".nsq"]:
-            try:
-                os.remove(f"{database}{ext}")
-            except FileNotFoundError:
-                pass
+        for record in records:
+            if record:
+                SeqIO.write(record, out_fasta, 'fasta')
 
 def main():
     parser = argparse.ArgumentParser(description='Process OrthoDB HMM output')
@@ -74,16 +69,23 @@ def main():
     parser.add_argument('-r', '--results', required=True, help='Path to the results file')
     parser.add_argument('-o', '--output', required=True, help='Path to the output file')
     parser.add_argument('-f', '--fasta', help='Path to the input FASTA file (optional)')
-    parser.add_argument('-d', '--db', help='Path to the existing BLAST database (optional)')
+    parser.add_argument('-t', '--threads', type=int, default=4, help='Number of threads (default: 4)')
     args = parser.parse_args()
 
-    # Replace IDs with species names in the results file
+    if not args.species_tab or not args.results or not args.output:
+        print("Error: Missing required arguments. Please check the usage and provide the necessary arguments.")
+        sys.exit(1)
+
+    if not args.fasta:
+        print("Warning: No FASTA file provided. Only replacing IDs in the results file.")
+    else:
+        print("Processing FASTA file...")
+
     replace_ids(args.species_tab, args.results, args.output)
 
-    if args.fasta or args.db:
-        # Extract sequences from the database (or FASTA file) and write them to an output FASTA file with updated headers
+    if args.fasta:
         output_fasta_file = args.output.rsplit('.', 1)[0] + '_sequences.fasta'
-        extract_fasta_sequences(args.db, args.fasta, args.species_tab, args.results, output_fasta_file)
+        extract_fasta_sequences(args.fasta, args.species_tab, args.results, output_fasta_file, args.threads)
 
 if __name__ == '__main__':
     main()
