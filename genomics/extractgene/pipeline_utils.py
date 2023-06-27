c2996ab07ab389610f8146b2e08f6efd786c813f
InverTome <104219631+invertome@users.noreply.github.com>
2023-06-27 11:24:41 -0600

    Update pipeline_utils.py

--- genomics/extractgene/pipeline_utils.py
+++ genomics/extractgene/pipeline_utils.py
@@ -13,9 +13,10 @@ def parse_input_files(mrna_fasta, genome_fasta):
     Read and parse input files using Biopython's SeqIO.parse.
     """
     mrna_sequences = list(SeqIO.parse(mrna_fasta, "fasta"))
-    genome_sequence = next(SeqIO.parse(genome_fasta, "fasta"))
+    genome_sequences = SeqIO.to_dict(SeqIO.parse(genome_fasta, "fasta"))
 
-    return mrna_sequences, genome_sequence
+    return mrna_sequences, genome_sequences
+
     
 def create_blast_database(genome_fasta):
     """
@@ -56,7 +57,7 @@ def perform_blast_search(mrna, genome_db, evalue):
     return blast_record
 
 
-def extract_upstream_sequences(blast_outputs, genome_sequence, upstream_length):
+def extract_upstream_sequences(blast_outputs, genome_sequences, upstream_length):
     extracted_sequences = []
 
     for i, blast_record in enumerate(blast_outputs):
@@ -64,28 +65,31 @@ def extract_upstream_sequences(blast_outputs, genome_sequence, upstream_length):
             alignment = blast_record.alignments[0]  # only take top alignment
             if alignment.hsps:
                 hsp = alignment.hsps[0]  # only take top hsp
+                reference_id = alignment.hit_def
 
-                # Check the orientation of the alignment
-                if hsp.frame[1] == 1:  # subject (genome) sequence is forward-oriented
-                    start = hsp.sbjct_start - upstream_length
-                    end = hsp.sbjct_start - 1
-                else:  # subject (genome) sequence is reverse-oriented
-                    start = hsp.sbjct_end + 1
-                    end = hsp.sbjct_end + upstream_length
+                if reference_id not in genome_sequences:
+                    logging.error(f"Reference {reference_id} not found in genome sequences.")
+                    continue
+
+                ref_seq = genome_sequences[reference_id]
+
+                start = hsp.sbjct_start - upstream_length
+                end = hsp.sbjct_start - 1
 
                 if start < 1:
                     start = 1
 
-                if start < end and end <= len(genome_sequence):  # check coordinates are valid
-                    extracted_seq = genome_sequence.seq[start - 1:end]
+                if start < end and end <= len(ref_seq):  # check coordinates are valid
+                    extracted_seq = ref_seq.seq[start - 1:end]
                     logging.info(f"Extracted sequence for {blast_record.query}: {extracted_seq}")  # Debug print statement
                     extracted_sequences.append((blast_record.query, extracted_seq))  # Keep original ID
                 else:
-                    logging.error(f"Invalid coordinates for {blast_record.query}: start={start}, end={end}, len={len(genome_sequence)}")
+                    logging.error(f"Invalid coordinates for {blast_record.query}: start={start}, end={end}, len={len(ref_seq)}")
         else:
             logging.warning(f"No alignments found for {blast_record.query}")  # Debug print statement
 
     return extracted_sequences
+
 
 
 def output_fasta(sequences, output_file):
