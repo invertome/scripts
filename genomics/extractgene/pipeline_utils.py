c73441f48e08bb9afdfb32d9390ddbb121cf390a
InverTome <104219631+invertome@users.noreply.github.com>
2023-06-27 11:01:56 -0600

    Update pipeline_utils.py

--- genomics/extractgene/pipeline_utils.py
+++ genomics/extractgene/pipeline_utils.py
@@ -3,6 +3,10 @@ from Bio import SeqIO
 from Bio.Blast.Applications import NcbiblastnCommandline, NcbimakeblastdbCommandline
 from Bio.Blast import NCBIXML
 import subprocess
+import logging
+from pathlib import Path
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 def parse_input_files(mrna_fasta, genome_fasta):
     """
@@ -13,28 +17,28 @@ def parse_input_files(mrna_fasta, genome_fasta):
 
     return mrna_sequences, genome_sequence
 
-def perform_blast_search(mrna, genome_fasta, blast_output_file, evalue):
+def perform_blast_search(mrna, genome_db, evalue):
     """
     Perform BLAST search using NcbiblastnCommandline.
     """
-    # Create BLAST database for the genome sequence with parsed sequence IDs
-    makeblastdb_cline = NcbimakeblastdbCommandline(dbtype="nucl", input_file=genome_fasta, parse_seqids=True)
-    makeblastdb_cline()
-
     # Create a temporary FASTA file for the current mRNA sequence
-    temp_mrna_fasta = os.path.join(os.path.dirname(genome_fasta), f"{mrna.id}_temp.fasta")
+    temp_mrna_fasta = Path(genome_db.parent, f"{mrna.id}_temp.fasta")
     SeqIO.write(mrna, temp_mrna_fasta, "fasta")
 
+    # Temporary output file
+    blast_output_file = Path(genome_db.parent, f"{mrna.id}_blast.xml")
+    
     # Perform the BLAST search
-    blastn_cline = NcbiblastnCommandline(query=temp_mrna_fasta, db=genome_fasta, evalue=evalue, outfmt=5, out=blast_output_file)
+    blastn_cline = NcbiblastnCommandline(query=str(temp_mrna_fasta), db=str(genome_db), evalue=evalue, outfmt=5, out=str(blast_output_file))
     blastn_cline()
 
-    # Remove the temporary FASTA file
-    os.remove(temp_mrna_fasta)
-
     # Parse the BLAST output
-    with open(blast_output_file, "r") as blast_output_handle:
+    with blast_output_file.open() as blast_output_handle:
         blast_record = NCBIXML.read(blast_output_handle)
+
+    # Clean up temporary files
+    temp_mrna_fasta.unlink()
+    blast_output_file.unlink()
 
     return blast_record
 
@@ -55,14 +59,14 @@ def extract_upstream_sequences(blast_outputs, genome_sequence, upstream_length):
 
                 if start < end and end <= len(genome_sequence):  # check coordinates are valid
                     extracted_seq = genome_sequence.seq[start - 1:end]
-                    print(f"Extracted sequence for {blast_record.query}: {extracted_seq}")  # Debug print statement
+                    logging.info(f"Extracted sequence for {blast_record.query}: {extracted_seq}")  # Debug print statement
                     extracted_sequences.append((blast_record.query, extracted_seq))  # Keep original ID
+                else:
+                    logging.error(f"Invalid coordinates for {blast_record.query}: start={start}, end={end}, len={len(genome_sequence)}")
         else:
-            print(f"No alignments found for {blast_record.query}")  # Debug print statement
+            logging.warning(f"No alignments found for {blast_record.query}")  # Debug print statement
 
     return extracted_sequences
-
-
 
 def output_fasta(sequences, output_file):
     with open(output_file, "w") as output_handle:
@@ -73,5 +77,5 @@ def predict_promoters(input_fasta_file, output_file):
     """
     Run promoter prediction using the Promoter2.0 tool.
     """
-    promoter_cline = f"promoter2 {input_fasta_file} > {output_file}"
+    promoter_cline = ["promoter2", input_fasta_file, ">", output_file]
     subprocess.run(promoter_cline, shell=True, check=True)
